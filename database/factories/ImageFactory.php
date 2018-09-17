91dd2d64fb7ffcb614a91328b167a5d56e4c5f10
mivkovic <markovicm9000@gmail.com>
2018-09-17 21:57:35 +0200

    factories and seeders

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -13,12 +13,8 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(App\Image::class, function (Faker $faker) {
     return [
-        'first_name' => $faker->firstName,
-        'last_name' => $faker->lastName,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'image_url' => $faker->imageUrl($width = 640, $height = 480)
     ];
 });
